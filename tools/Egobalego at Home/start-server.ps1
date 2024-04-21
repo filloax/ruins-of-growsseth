0f367d79a8193fc595ca8ae16965da2fe8262e1b
costantino2000 <86307814+costantin0@users.noreply.github.com>
2024-04-21 22:13:49 +0200

    Fixed powershell script for the web console
    + updated the forge id

--- tools/Egobalego at Home/start-server.ps1
+++ tools/Egobalego at Home/start-server.ps1
@@ -4,7 +4,8 @@ if (-not $pythonExists) {
     Write-Host "Please install Python 3.x https://www.python.org/downloads/"
     exit 1
 }
-$python = python --version 2>&1
+
+$python = py --version 2>&1
 if ($python -notmatch "Python") {
     Write-Host "Please install Python 3.x https://www.python.org/downloads/"
     exit 1
@@ -13,7 +14,7 @@ if ($python -notmatch "Python") {
 $venv = $PSScriptRoot + "\.venv"
 if (!(Test-Path $venv)) {
     Write-Host "Virtual environment does not exist. Creating now."
-    python -m venv $venv
+    py -m venv $venv
 }
 
 "$venv\Scripts\Activate.ps1"
@@ -22,5 +23,6 @@ if (!(Test-Path $venv)) {
 pip install -r "$PSScriptRoot\requirements.txt"
 
 $script = $PSScriptRoot + "\start-server.py"
+echo $script
 
-python $script --open
+py $script --open
