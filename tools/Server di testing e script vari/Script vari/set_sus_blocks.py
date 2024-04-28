1f376b79788c8c3d2d1fa668e37008faf58452c8
costantino2000 <86307814+costantin0@users.noreply.github.com>
2024-04-28 17:42:40 +0200

    Reapplied scripts to conduit ruins and church
    + removed unused zombie researcher loot table

--- tools/Server di testing e script vari/Script vari/set_sus_blocks.py
+++ tools/Server di testing e script vari/Script vari/set_sus_blocks.py
@@ -3,22 +3,21 @@ import os
 
 os.chdir(os.path.dirname(__file__))
 
-# This script is deprecated because the new conduit ruins have special characters in the signs, use the notebook instead
-
-file = "../../Mod Fabric/src/main/resources/data/growsseth/structures/ruins/conduit_ruins/main.nbt"
+files = [
+    "../../../src/main/resources/data/growsseth/structures/ruins/conduit_ruins.nbt",
+    "../../../src/main/resources/data/growsseth/structures/ruins/conduit_church/main.nbt"
+]
 loot_table = "growsseth:conduit_ruins_archaeology"
 
-nbtfile = nbt.NBTFile(file)
 
-count = 0
-
-for block in nbtfile["blocks"]:
-    if "nbt" in block:
-        block_entity = block["nbt"]
-        if block_entity['id'].value == "minecraft:brushable_block":
-            block_entity["LootTable"] = nbt.TAG_String(loot_table)
-            count += 1
-
-nbtfile.write_file()
-
-print(f"Converted {count} sus blocks")
+for file in files:
+    count = 0
+    nbtfile = nbt.NBTFile(file)
+    for block in nbtfile["blocks"]:
+        if "nbt" in block:
+            block_entity = block["nbt"]
+            if block_entity['id'].value == "minecraft:brushable_block":
+                block_entity["LootTable"] = nbt.TAG_String(loot_table)
+                count += 1
+    nbtfile.write_file()
+    print(f"Converted {count} sus blocks")
