b24d43f7e2d74312c8d9ae21739f13c5beff43c8
filloax <filloax@gmail.com>
2024-04-30 16:04:58 +0200

    auto server language

--- src/client/kotlin/com/ruslan/growsseth/config/ClientConfigHandler.kt
+++ src/client/kotlin/com/ruslan/growsseth/config/ClientConfigHandler.kt
@@ -1,12 +1,24 @@
 package com.ruslan.growsseth.config
 
-import com.ruslan.growsseth.RuinsOfGrowsseth
 import com.teamresourceful.resourcefulconfig.client.ConfigScreen
+import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.screens.Screen
 
 object ClientConfigHandler {
     fun configScreen(parent: Screen?): ConfigScreen? {
         val config = GrowssethConfigHandler.config
         return config?.let{ ConfigScreen(parent, config) }
+    }
+
+    fun initClientConfig() {
+        GrowssethConfigHandler.onConfigLoad {
+            if (GrowssethConfig.serverLanguage == "client") {
+                val lang = Minecraft.getInstance().languageManager.selected
+                GrowssethConfig.serverLanguage = if (GrowssethConfigHandler.INCLUDED_LANGUAGES.contains(lang))
+                    lang
+                else
+                    GrowssethConfigHandler.DEFAULT_LANGUAGE
+            }
+        }
     }
 }
