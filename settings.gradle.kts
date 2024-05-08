4e00a98a87eb2595315a5b8eb6d07e468b81193c
filloax <filloax@gmail.com>
2024-05-08 10:37:38 +0200

    version: 1.20.6 gradle changes (Also port structure changes to 1.20.4)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,10 @@ pluginManagement {
     }
 
     val kotlinVersion: String by settings
+    val loomVersion: String by settings
     plugins {
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
+        id("fabric-loom") version loomVersion
     }
 }
