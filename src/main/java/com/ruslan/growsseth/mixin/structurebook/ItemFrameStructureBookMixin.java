7ae68ad9d1ff1ca61ae72cea1a7a33159ae8a99e
costantino2000 <86307814+costantin0@users.noreply.github.com>
2024-05-08 11:11:32 +0200

    Improved item frame book conversion

--- src/main/java/com/ruslan/growsseth/mixin/structurebook/ItemFrameStructureBookMixin.java
+++ src/main/java/com/ruslan/growsseth/mixin/structurebook/ItemFrameStructureBookMixin.java
@@ -1,28 +1,30 @@
 package com.ruslan.growsseth.mixin.structurebook;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.ruslan.growsseth.structure.StructureBooks;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.decoration.ItemFrame;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemFrame.class)
 public abstract class ItemFrameStructureBookMixin {
-    @Unique
-    ItemFrame thisItemFrame = (ItemFrame)(Object)this;
 
-    @Inject(method = "dropItem(Lnet/minecraft/world/entity/Entity;Z)V", at=@At("HEAD"))
-    private void convertDroppedBook(Entity entity, boolean dropSelf, CallbackInfo ci){
-        ItemStack item = thisItemFrame.getItem();
-        if (item.is(Items.WRITABLE_BOOK) || item.is(Items.WRITTEN_BOOK)){
-            if (StructureBooks.bookIsTemplate(item)) {
-                thisItemFrame.setItem(StructureBooks.loadTemplate(item));
-            }
+    @WrapOperation(
+        method = "readAdditionalSaveData",
+        at = @At(
+                value = "INVOKE",
+                target = "Lnet/minecraft/world/item/ItemStack;of(Lnet/minecraft/nbt/CompoundTag;)Lnet/minecraft/world/item/ItemStack;"
+        )
+    )
+    private ItemStack convertDroppedBook(CompoundTag compoundTag, Operation<ItemStack> original){
+        var item = original.call(compoundTag);
+        if ((item.is(Items.WRITABLE_BOOK) || item.is(Items.WRITTEN_BOOK)) && StructureBooks.bookIsTemplate(item)) {
+            return StructureBooks.loadTemplate(item);
         }
+        return item;
     }
 }
