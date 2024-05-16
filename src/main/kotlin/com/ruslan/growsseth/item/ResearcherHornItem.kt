ecc0ec1a42f26b89f8aeee607af8c77a9ded7747
costantino2000 <86307814+costantin0@users.noreply.github.com>
2024-05-16 20:25:26 +0200

    Removed researcher horn description when not given by quest

--- src/main/kotlin/com/ruslan/growsseth/item/ResearcherHornItem.kt
+++ src/main/kotlin/com/ruslan/growsseth/item/ResearcherHornItem.kt
@@ -1,29 +1,7 @@
 package com.ruslan.growsseth.item
 
-import net.minecraft.ChatFormatting
-import net.minecraft.network.chat.Component
-import net.minecraft.network.chat.Style
 import net.minecraft.tags.TagKey
 import net.minecraft.world.item.Instrument
 import net.minecraft.world.item.InstrumentItem
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.TooltipFlag
-import net.minecraft.world.level.Level
 
-class ResearcherHornItem(properties: Properties, instruments: TagKey<Instrument>) : InstrumentItem(properties, instruments) {
-    override fun appendHoverText(
-        stack: ItemStack,
-        context: TooltipContext,
-        tooltipComponents: MutableList<Component>,
-        isAdvanced: TooltipFlag
-    ) {
-        for (i in 1 .. 2) {
-            tooltipComponents.add(Component.translatable("${stack.descriptionId}.description$i")
-                .withStyle(Style.EMPTY
-                    .applyFormat(ChatFormatting.BLUE)
-                    .applyFormat(ChatFormatting.ITALIC)
-                ))
-        }
-        super.appendHoverText(stack, context, tooltipComponents, isAdvanced)
-    }
-}
+class ResearcherHornItem(properties: Properties, instruments: TagKey<Instrument>) : InstrumentItem(properties, instruments)
