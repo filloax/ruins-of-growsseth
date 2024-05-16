a05f5b32a1ad1229845ef43029e542ca9548eacc
filloax <filloax@gmail.com>
2024-05-16 16:39:04 +0200

    no flooding in jigsaws using fxlib
    Removed researcher horn description when not given by quest
    
    Updated growsseth places
    
    Fixed donkey chest and leash
    
    fix researcher through bars, progress
    
    researcher sells everything before leaving, to everyone
    
    final quest leave supports both daytime (commands)
    and consistent server time,
    double check progress trades progress every 5 seconds
    
    record authors setup (no athors for most discs yet)

--- src/main/kotlin/com/ruslan/growsseth/ModEvents.kt
+++ src/main/kotlin/com/ruslan/growsseth/ModEvents.kt
@@ -97,6 +97,7 @@ abstract class ModEvents {
         onStartServerTick { server ->
             DataRemoteSync.checkTickSync(WebConfig.dataSyncUrl, server)
             GrowssethAdvancements.Callbacks.onServerTick(server)
+            ProgressResearcherTradesProvider.Callbacks.onServerTick(server)
         }
 
         onEndServerLevelTick { level ->
