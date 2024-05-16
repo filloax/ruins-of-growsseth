66022fa7a4477799a999356a0bdb424271f6c7b5
costantino2000 <86307814+costantin0@users.noreply.github.com>
2024-05-16 12:23:35 +0200

    Fixed discs ordering for growsseth creative tab

--- src/main/kotlin/com/ruslan/growsseth/data/DataGeneration.kt
+++ src/main/kotlin/com/ruslan/growsseth/data/DataGeneration.kt
@@ -266,7 +266,7 @@ class ModelGenerator constructor(generator: FabricDataOutput) : FabricModelProvi
                         itemModelGenerator.generateLayeredItem(model, ResourceLocation("item/music_disc_pigstep"), discsVocalsLayer)
                     in GrowssethItems.DISCS_TO_VOCALS.values ->
                         itemModelGenerator.generateLayeredItem(model, discsBaseLayer, discsVocalsLayer, discsSongLayer)
-                    in GrowssethItems.DISCS_INSTRUMENTAL, in GrowssethItems.DISCS_WITH_VOCALS ->
+                    in GrowssethItems.DISCS_ORDERED ->
                         itemModelGenerator.generateLayeredItem(model, discsBaseLayer, discsSongLayer)
                     else ->
                         itemModelGenerator.generateFlatItem(item, ModelTemplates.FLAT_ITEM)
