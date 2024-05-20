db0ed17ae16d7f10e5a06f571c314079fe17f1ec
filloax <filloax@gmail.com>
2024-05-20 15:50:30 +0200

    dialogues are custom packet
    (MIGHT fix multiplayer bug)

--- src/client/kotlin/com/ruslan/growsseth/client/network/GrowssethNetworkingClient.kt
+++ src/client/kotlin/com/ruslan/growsseth/client/network/GrowssethNetworkingClient.kt
@@ -7,6 +7,7 @@ import com.ruslan.growsseth.client.worldpreset.GrowssethWorldPresetClient
 import com.ruslan.growsseth.config.ClientConfig
 import com.ruslan.growsseth.config.GrowssethConfig
 import com.ruslan.growsseth.config.ResearcherConfig
+import com.ruslan.growsseth.dialogues.handleNpcDialogueLine
 import com.ruslan.growsseth.networking.*
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
 import net.minecraft.client.Minecraft
@@ -16,6 +17,11 @@ import net.minecraft.sounds.SoundEvents
 object GrowssethNetworkingClient {
     fun init() {
         val client = Minecraft.getInstance()
+
+        ClientPlayNetworking.registerGlobalReceiver(DialoguePacket.TYPE) { packet, context ->
+            client.player?.handleNpcDialogueLine(packet)
+        }
+
         ClientPlayNetworking.registerGlobalReceiver(ResearcherTradesNotifPacket.TYPE) { packet, context ->
             if (ClientConfig.newTradeNotifications) {
                 client.toasts.updateNewTradeToast(packet.newTrades)
