0222fa7dd08d98378f15dc7d2fce517112272915
costantino2000 <86307814+costantin0@users.noreply.github.com>
2024-05-21 16:15:50 +0200

    Researcher does not appear anymore if killed
    and single researcher is true of course

--- src/main/kotlin/com/ruslan/growsseth/entity/researcher/ResearcherSavedData.kt
+++ src/main/kotlin/com/ruslan/growsseth/entity/researcher/ResearcherSavedData.kt
@@ -12,19 +12,20 @@ import kotlin.jvm.optionals.getOrNull
 
 /**
  * Save researcher data in the level to allow it to persist even if he gets respawned.
- * Mainly meant for the livestream ver, can be tweaked to work in the commmunity ver too.
  */
 class ResearcherSavedData private constructor (
     var data: CompoundTag = CompoundTag(),
     var name: Component? = null,
+    var isDead: Boolean = false,
 ) : FxSavedData<ResearcherSavedData>(CODEC) {
 
     companion object {
         val CODEC: Codec<ResearcherSavedData> = RecordCodecBuilder.create { builder -> builder.group(
             CompoundTag.CODEC.fieldOf("data").forGetter(ResearcherSavedData::data),
             ComponentSerialization.CODEC.optionalFieldOf("name").forNullableGetter(ResearcherSavedData::name),
-        ).apply(builder) { data, name ->
-            ResearcherSavedData(data, name.getOrNull())
+            Codec.BOOL.fieldOf("isDead").forGetter(ResearcherSavedData::isDead)
+        ).apply(builder) { data, name, isDead ->
+            ResearcherSavedData(data, name.getOrNull(), isDead)
         } }
 
         private val DEF = define("researcher_data", ::ResearcherSavedData, CODEC)
