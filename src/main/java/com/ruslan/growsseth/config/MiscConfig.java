25a3863fdd8a57269939c8029187146111d6634c
costantino2000 <86307814+costantino2000@users.noreply.github.com>
2024-05-26 16:29:00 +0200

    Added setting for npcs dialogue speed

--- src/main/java/com/ruslan/growsseth/config/MiscConfig.java
+++ src/main/java/com/ruslan/growsseth/config/MiscConfig.java
@@ -13,6 +13,10 @@ public class MiscConfig {
     @Comment(value = "If changed ingame, takes effect on world reload", translation = T_PREF + "needsWorldReload.comment")
     public static boolean modLootInVanillaStructures = false;
 
+    @ConfigEntry(id = "dialogueWordsPerMinute", type = EntryType.INTEGER, translation = T_PREF + "dialogueWordsPerMinute.name")
+    @Comment(value = "Reading speed (in words per minute) of chat messages, used for multiline npc dialogues. Increase it to make npcs 'talk' faster, decrease it to make them 'talk' slower. Set it to 0 to receive all messages together.", translation = T_PREF + "dialogueWordsPerMinute.comment")
+    public static int dialogueWordsPerMinute = 120;
+
     @ConfigEntry(id = "zombieGuberSpawnChance", type = EntryType.FLOAT, translation = T_PREF + "zombieGuberSpawnChance.name")
     public static float zombieGuberSpawnChance = 0.2f;
 }
