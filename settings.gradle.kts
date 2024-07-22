9ceb26e6cdce54e2ffc1135969487a90d3a4da61
filloax <filloax@gmail.com>
2024-07-22 22:37:16 +0200

    1.20.6: fix libs, start using gradle libs toml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,12 +6,4 @@ pluginManagement {
         }
         gradlePluginPortal()
     }
-
-    val kotlinVersion: String by settings
-    val loomVersion: String by settings
-    plugins {
-        kotlin("jvm") version kotlinVersion
-        kotlin("plugin.serialization") version kotlinVersion
-        id("fabric-loom") version loomVersion
-    }
 }
