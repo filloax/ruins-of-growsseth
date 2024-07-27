4502bbc32ec877de40a1ad028b3ef51342563191
filloax <filloax@gmail.com>
2024-07-27 14:13:59 +0200

    1.21: fix disc credits order

--- src/main/kotlin/com/ruslan/growsseth/item/GrowssethJukeboxSongs.kt
+++ src/main/kotlin/com/ruslan/growsseth/item/GrowssethJukeboxSongs.kt
@@ -1,7 +1,9 @@
 package com.ruslan.growsseth.item
 
+import com.filloax.fxlib.api.FxUtils
 import com.ruslan.growsseth.sound.GrowssethSounds
 import com.ruslan.growsseth.utils.resLoc
+import net.minecraft.ChatFormatting
 import net.minecraft.Util
 import net.minecraft.core.Holder
 import net.minecraft.core.registries.Registries
@@ -12,24 +14,115 @@ import net.minecraft.sounds.SoundEvent
 import net.minecraft.world.item.JukeboxSong
 
 object GrowssethJukeboxSongs {
-    val SEGA_DI_NIENTE 		= create("sega_di_niente")
+    val SEGA_DI_NIENTE 		    = create("sega_di_niente")
     val GIORGIO_CUBETTI 		= create("giorgio_cubetti")
     val GIORGIO_LOFI 			= create("giorgio_lofi")
     val GIORGIO_LOFI_INST 		= create("giorgio_lofi_inst")
     val GIORGIO_FINDING_HOME 	= create("giorgio_finding_home")
     val GIORGIO_8BIT		 	= create("giorgio_8bit")
-    val BINOBINOOO 			= create("binobinooo")
+    val BINOBINOOO 			    = create("binobinooo")
     val PADRE_MAMMONK 			= create("padre_mammonk")
     val ABBANDONATI 			= create("abbandonati")
-    val MISSIVA_NELL_OMBRA 	= create("missiva_nell_ombra")
+    val MISSIVA_NELL_OMBRA 	    = create("missiva_nell_ombra")
     val OURSTEPS 				= create("oursteps")
-    val ODI_ET_AMO 			= create("odi_et_amo")
-    val ANCORA_QUI 			= create("ancora_qui")
+    val ODI_ET_AMO 			    = create("odi_et_amo")
+    val ANCORA_QUI 			    = create("ancora_qui")
     val BALLATA_DEL_RESPAWN 	= create("ballata_del_respawn")
     val CACO_CACO 				= create("caco_caco")
     val PESCI_STRANI 			= create("pesci_strani")
 
     private fun create(id: String) = ResourceKey.create(Registries.JUKEBOX_SONG, resLoc(id))
+
+    // Extended credits
+    @JvmField
+    val credits = mapOf(
+        SEGA_DI_NIENTE to Credits(listOf(
+                "Vocals & Lyrics - Cydonia",
+                "Mixing - Guber",
+                "Cover of 'Labyrinthine', by Lena Raine"
+            )),
+        GIORGIO_CUBETTI to Credits(listOf(
+                "Vocals, Mix & Master - Singalek",
+                "Lyrics - Singalek, Cydonia",
+                "Cover of 'If I Could, I Would' - MINDME"
+            ),
+            ytChannel = "SingalekSMW"
+        ),
+        GIORGIO_LOFI to Credits(listOf(
+                "Instrumental, Vocals, Mix, Master - Singalek",
+                "Lyrics - Singalek, Cydonia"
+            ),
+            ytChannel = "SingalekSMW"
+        ),
+        GIORGIO_LOFI_INST to Credits(ytChannel = "SingalekSMW"),
+        GIORGIO_FINDING_HOME to Credits(ytChannel = "HunterProduction"),
+        GIORGIO_8BIT to Credits(listOf(
+                "Music - Hawkelele",
+                "Based on 'Giorgio Cubetti' by Singalek",
+                "Cover of 'If I Could, I Would' - MINDME"
+            ),
+            ytChannel = "IlCoroDiMammonk"
+        ),
+        BINOBINOOO to Credits(ytChannel = "SingalekSMW"),
+        PADRE_MAMMONK to Credits(listOf(
+                "Vocals - Emoon LeStrange, HunterProduction, KalarFenrir, Singalek",
+                "Chorus composed by Emoon LeStrange",
+                "Cover of 'All is Found' - Evan Rachel Wood"
+            ),
+            ytChannel = "IlCoroDiMammonk"
+        ),
+        ABBANDONATI to Credits(listOf(
+                "Lyrics - Cydonia",
+                "Vocals - Ako",
+                "Music, Mix & Master - R-E-M"
+            ),
+            ytChannel = "rem.producer"
+        ),
+        MISSIVA_NELL_OMBRA to Credits(listOf(
+                "Vocals - Singalek",
+                "Music - Hawkelele, Monnui, AndreWharn, HunterProduction, 't Hooft",
+                "Cover of 'Sleepsong' - Secret Garden"
+            ),
+            ytChannel = "IlCoroDiMammonk"
+        ),
+        OURSTEPS to Credits(listOf(
+                "Vocals - Singalek, Blessing Of No One",
+                "Music - Lena Raine, IridioSound, Avage",
+                "Cover of 'Pigstep' - Lena Raine"
+            ),
+            ytChannel = "IlCoroDiMammonk"
+        ),
+        ODI_ET_AMO to Credits(listOf(
+                "Vocals - Jova117, Emoon LeStrange",
+                "Music - Hawkelele, Jova117"
+            ),
+            ytChannel = "IlCoroDiMammonk"
+        ),
+        ANCORA_QUI to Credits(listOf(
+                "Vocals - Avage, Emoon LeStrange, Singalek",
+                "Music - Avage"
+            ),
+            ytChannel = "IlCoroDiMammonk"
+        ),
+        BALLATA_DEL_RESPAWN to Credits(listOf(
+                "Vocals - Monnui",
+                "Music - Monnui, Singalek, IridioSound"
+            ),
+            ytChannel = "IlCoroDiMammonk"
+        ),
+        CACO_CACO to Credits(listOf(
+                "Vocals - Blessing Of No One, HunterProduction, KalarFenrir, Singalek",
+                "Music - HunterProduction"
+            ),
+            ytChannel = "IlCoroDiMammonk"
+        ),
+        PESCI_STRANI to Credits(listOf(
+                "Vocals - Emoon LeStrange, Singalek",
+                "Music - Hawkelele, Emoon LeStrange, Monnui, Singalek"
+            ),
+            ytChannel = "IlCoroDiMammonk"
+        )
+    )
 
     // Create data in datagen
     fun bootstrap(ctx: BootstrapContext<JukeboxSong>) {
@@ -64,5 +157,50 @@ object GrowssethJukeboxSongs {
                 lengthInSeconds.toFloat(), comparatorOutput
             )
         )
+    }
+
+    data class Credits(val credits: List<String> = listOf(), val ytChannel: String? = null) {
+        fun getTooltip(): List<Component>? {
+            if (credits.isEmpty() && ytChannel == null) return null
+            val out = mutableListOf<Component>()
+
+            if (FxUtils.hasShiftDown()) {
+                out.addAll(credits
+                    .flatMap { splitStringToMaxLength(it, 30).mapIndexed { index, s -> if (index >= 1) "  $s" else s } }
+                    .map { Component.literal(it).withStyle(ChatFormatting.BLUE) }
+                )
+                if (ytChannel != null) {
+                    out.add(Component.literal("YT: @$ytChannel").withStyle(ChatFormatting.RED))
+                }
+            } else {
+                out.add(Component.translatable("item.growsseth.authors.pressShift").withStyle(
+                    ChatFormatting.BLUE))
+            }
+            return out
+        }
+
+        private fun splitStringToMaxLength(input: String, maxLength: Int): List<String> {
+            val words = input.split(" ")
+            val lines = mutableListOf<String>()
+            var currentLine = ""
+
+            for (word in words) {
+                if (currentLine.length + word.length > maxLength) {
+                    lines.add(currentLine.trim())
+                    currentLine = word
+                } else {
+                    if (currentLine.isNotEmpty()) {
+                        currentLine += " "
+                    }
+                    currentLine += word
+                }
+            }
+
+            if (currentLine.isNotEmpty()) {
+                lines.add(currentLine.trim())
+            }
+
+            return lines
+        }
     }
 }
