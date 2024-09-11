f5280d31aa5581dc1a1f77559290a3f6b0e837f5
costantino2000 <86307814+costantin0@users.noreply.github.com>
2024-09-12 00:13:38 +0200

    researcher sounds separated from vanilla
    now resource packs won't override them
    + fixed double sound when refusing trade for no server connection

--- src/main/kotlin/com/ruslan/growsseth/sound/GrowssethSounds.kt
+++ src/main/kotlin/com/ruslan/growsseth/sound/GrowssethSounds.kt
@@ -31,6 +31,13 @@ class GrowssethSounds {
         val DEFLECT_ARROW_SOUND = registerSoundEvent("deflect_arrow_sound")
         val RESEARCHER_HORN_SOUND = registerSoundEventHolder("researcher_horn_sound")
 
+        val RESEARCHER_YES = registerSoundEvent("researcher.yes")
+        val RESEARCHER_NO = registerSoundEvent("researcher.no")
+        val RESEARCHER_TRADE = registerSoundEvent("researcher.trade")
+        val RESEARCHER_AMBIENT = registerSoundEvent("researcher.ambient")
+        val RESEARCHER_HURT = registerSoundEvent("researcher.hurt")
+        val RESEARCHER_DEATH = registerSoundEvent("researcher.death")
+
         private fun registerSoundEvent(name: String): SoundEvent {
             val id = ResourceLocation(RuinsOfGrowsseth.MOD_ID, name)
             return Registry.register(BuiltInRegistries.SOUND_EVENT, id, SoundEvent.createVariableRangeEvent(id))
