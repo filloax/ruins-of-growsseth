2a9f894dd02dcdae4d81be23f43f140db7e42e84
costantino2000 <costa2000nieddu@gmail.com>
2024-09-16 18:53:59 +0200

    updated researcherTeleports config description

--- src/main/java/com/ruslan/growsseth/config/ResearcherConfig.java
+++ src/main/java/com/ruslan/growsseth/config/ResearcherConfig.java
@@ -54,7 +54,7 @@ public final class ResearcherConfig {
 
     @ConfigEntry(id = "researcherTeleports", type = EntryType.BOOLEAN, translation = T_PREF + "researcherTeleports.name")
     @Comment(
-            value = "The researcher teleports back to his original position after five minutes away from it, and avoids interacting with Nether and End portals to prevent cheese.",
+            value = "The researcher teleports back to his original position after five minutes away from it (or after suffocating for 3 seconds inside a wall), and avoids interacting with Nether and End portals to prevent cheese.",
             translation = T_PREF + "researcherTeleports.comment"
     )
     public static boolean researcherTeleports = true;
