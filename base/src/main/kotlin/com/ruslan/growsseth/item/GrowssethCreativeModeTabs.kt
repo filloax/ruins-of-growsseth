6845d43d6923a71db01de808a75cb3310ed14536
filloax <filloax@gmail.com>
2024-10-15 16:44:30 +0200

    creative mode tabs

--- base/src/main/kotlin/com/ruslan/growsseth/item/GrowssethCreativeModeTabs.kt
+++ base/src/main/kotlin/com/ruslan/growsseth/item/GrowssethCreativeModeTabs.kt
@@ -1,38 +1,44 @@
 package com.ruslan.growsseth.item
 
 import com.ruslan.growsseth.RuinsOfGrowsseth
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
+import com.ruslan.growsseth.utils.resLoc
 import net.minecraft.core.Registry
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.CreativeModeTab
+import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.levelgen.structure.pieces.StructurePieceType
 
 
 object GrowssethCreativeModeTabs {
+    private val all = mutableMapOf<ResourceLocation, CreativeModeTab>()
 
-    // TODO: double issue of registry register not being directly callable in non-fabric, and fabricitemgroup usage
-
-    val GROWSSETH_ITEMS: CreativeModeTab = Registry.register(
-        BuiltInRegistries.CREATIVE_MODE_TAB,
-        ResourceLocation.fromNamespaceAndPath(RuinsOfGrowsseth.MOD_ID, "growsseth"),
-        FabricItemGroup.builder().title(Component.translatable("growsseth.creative_tab"))
-            .icon{ItemStack(GrowssethItems.RESEARCHER_HORN)}
-            .displayItems{ params, output ->
-                output.accept(GrowssethItems.RESEARCHER_SPAWN_EGG)
-                output.accept(GrowssethItems.ZOMBIE_RESEARCHER_SPAWN_EGG)
-                output.accept(GrowssethItems.RESEARCHER_DAGGER)
-                output.accept(GrowssethItems.RESEARCHER_HORN)
-                output.accept(GrowssethItems.GROWSSETH_BANNER_PATTERN)
-                output.accept(GrowssethItems.GROWSSETH_ARMOR_TRIM)
-                output.accept(GrowssethItems.GROWSSETH_POTTERY_SHERD)
-                output.accept(GrowssethItems.FRAGMENT_BALLATA_DEL_RESPAWN)
-                for (disc in GrowssethItems.DISCS_ORDERED) {
-                    output.accept(disc)
-                }
-            }.build()
+    val GROWSSETH_ITEMS = make("growsseth", CreativeModeTab.builder(CreativeModeTab.Row.TOP, 0)
+        .title(Component.translatable("growsseth.creative_tab"))
+        .icon{ItemStack(GrowssethItems.RESEARCHER_HORN)}
+        .displayItems{ _, output ->
+            output.accept(GrowssethItems.RESEARCHER_SPAWN_EGG)
+            output.accept(GrowssethItems.ZOMBIE_RESEARCHER_SPAWN_EGG)
+            output.accept(GrowssethItems.RESEARCHER_DAGGER)
+            output.accept(GrowssethItems.RESEARCHER_HORN)
+            output.accept(GrowssethItems.GROWSSETH_BANNER_PATTERN)
+            output.accept(GrowssethItems.GROWSSETH_ARMOR_TRIM)
+            output.accept(GrowssethItems.GROWSSETH_POTTERY_SHERD)
+            output.accept(GrowssethItems.FRAGMENT_BALLATA_DEL_RESPAWN)
+            for (disc in GrowssethItems.DISCS_ORDERED) {
+                output.accept(disc)
+            }
+        }.build()
     )
 
-    fun registerCreativeModeTabs() {}
+    fun registerCreativeModeTabs(registrator: (ResourceLocation, CreativeModeTab) -> Unit) {
+        all.forEach { t, u -> registrator(t, u) }
+    }
+
+    private fun make(id: String, creativeModeTab: CreativeModeTab): CreativeModeTab {
+        all[resLoc(id)] = creativeModeTab
+        return creativeModeTab
+    }
 }
