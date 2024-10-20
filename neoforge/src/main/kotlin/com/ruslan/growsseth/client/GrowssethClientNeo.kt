3b33b029c8b5ca4e4b96490b1d8488ec27c77af2
filloax <filloax@gmail.com>
2024-10-20 13:04:21 +0200

    neo: reload listeners & client events

--- neoforge/src/main/kotlin/com/ruslan/growsseth/client/GrowssethClientNeo.kt
+++ neoforge/src/main/kotlin/com/ruslan/growsseth/client/GrowssethClientNeo.kt
@@ -2,33 +2,41 @@ package com.ruslan.growsseth.client
 
 import com.filloax.fxlib.FxLib
 import com.ruslan.growsseth.RuinsOfGrowsseth
+import com.ruslan.growsseth.client.resource.EncryptedMusicResources
+import com.ruslan.growsseth.client.worldpreset.GrowssethWorldPresetClient
 import com.ruslan.growsseth.config.ClientConfigHandler
 import com.teamresourceful.resourcefulconfig.client.ConfigScreen
+import net.minecraft.client.Minecraft
 
 import net.minecraft.client.gui.screens.Screen
+import net.neoforged.bus.api.SubscribeEvent
+import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
+import net.neoforged.neoforge.client.event.ClientTickEvent
+import net.neoforged.neoforge.client.event.RegisterClientReloadListenersEvent
 import org.apache.logging.log4j.Level
+import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 
-
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 object GrowssethClientNeo {
     fun initializeClient(event: FMLClientSetupEvent) {
         FxLib.logger.info("Initializing client...")
         GrowssethRenderers.init()
         GrowssethItemsClient.init()
 
-        // TODO
-//        ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(EncryptedMusicResources.KeyListener() as IdentifiableResourceReloadListener)
-
         initEvents()
 
         RuinsOfGrowsseth.log(Level.INFO, "Initialized Client!")
     }
 
-    private fun initEvents() {
-        // TODO
-//        ClientTickEvents.START_CLIENT_TICK.register { client ->
-//            GrowssethWorldPresetClient.Callbacks.onClientTick(client)
-//        }
+    @SubscribeEvent
+    fun registerResourceReloadListeners(ev: RegisterClientReloadListenersEvent) {
+        ev.registerReloadListener(EncryptedMusicResources.KeyListener())
+    }
+
+    @SubscribeEvent
+    fun clientTickEventPre(ev: ClientTickEvent.Pre) {
+        GrowssethWorldPresetClient.Callbacks.onClientTick(Minecraft.getInstance())
     }
 
     @JvmStatic
