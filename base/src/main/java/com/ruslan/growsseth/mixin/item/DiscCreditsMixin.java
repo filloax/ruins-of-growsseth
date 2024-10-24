cc7ce9cd7bea0c0727760498e96c7cbe4ecff659
filloax <filloax@gmail.com>
2024-10-24 13:45:18 +0200

    fix synthethic references in mixins to work cross-loader

--- base/src/main/java/com/ruslan/growsseth/mixin/item/DiscCreditsMixin.java
+++ base/src/main/java/com/ruslan/growsseth/mixin/item/DiscCreditsMixin.java
@@ -15,7 +15,12 @@ import java.util.function.Consumer;
 
 @Mixin(JukeboxPlayable.class)
 public class DiscCreditsMixin {
-    @Inject(method = "method_60748", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V"))
+    // Inside addToTooltip
+    @SuppressWarnings("UnresolvedMixinReference")
+    @Inject(
+        method = { "lambda$addToTooltip$1", "method_60748" },
+        at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V")
+    )
     private static void addExtraCredits(Consumer<Component> tooltipAdder, Holder<JukeboxSong> holder, CallbackInfo ci) {
         if (holder.unwrapKey().map(GrowssethJukeboxSongs.credits::containsKey).orElse(false)) {
             var credits = GrowssethJukeboxSongs.credits.get(holder.unwrapKey().orElseThrow());
