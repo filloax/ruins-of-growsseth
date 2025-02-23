d69726ead44791c754060246155822f6b41afe06
costantino2000 <86307814+costantin0@users.noreply.github.com>
2025-02-23 21:24:54 +0100

    fixed logDev log, square brackets for logs

--- src/main/kotlin/com/ruslan/growsseth/RuinsOfGrowsseth.kt
+++ src/main/kotlin/com/ruslan/growsseth/RuinsOfGrowsseth.kt
@@ -145,7 +145,7 @@ object RuinsOfGrowsseth : ModInitializer {
 
     fun logDev(level: Level, message: String) {
         if (FabricLoader.getInstance().isDevelopmentEnvironment) {
-            LOGGER.log(level, "[$MOD_NAME] $message")
+            LOGGER.log(level, message)
         }
     }
 
