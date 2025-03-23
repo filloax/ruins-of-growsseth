50341002823f46d133e346ba293a6a0c2706cb1b
filloax <filloax@gmail.com>
2025-03-24 00:44:04 +0100

    meta: versionSuffix -> versionType

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,3 @@
 plugins {
     alias(libs.plugins.ideaext)
 }
-
-// For common build.gradle, do not load plugin but use catalogs directly
-val modVersion: String by project
-
-tasks.register("modVersion") {
-    println("VERSION=$modVersion")
-}
