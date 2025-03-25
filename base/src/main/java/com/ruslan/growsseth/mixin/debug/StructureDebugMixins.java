322e44065592a935f2dd7b33b83c89158f2d2c58
filloax <filloax@gmail.com>
2025-03-26 00:39:43 +0100

    logger to field for java, fix debug structures (see details)
    Apparently in MC versions the registry code that the debug structures setting affected changed to use Optional<StructureSet> where it used StructureSet, so it broke (not detectable at compile time due to our casts)

--- base/src/main/java/com/ruslan/growsseth/mixin/debug/StructureDebugMixins.java
+++ base/src/main/java/com/ruslan/growsseth/mixin/debug/StructureDebugMixins.java
@@ -10,7 +10,9 @@ import com.ruslan.growsseth.RuinsOfGrowsseth;
 import com.ruslan.growsseth.config.DebugConfig;
 import net.minecraft.core.*;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.Tag;
 import net.minecraft.resources.RegistryDataLoader;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.BiomeGenerationSettings;
 import net.minecraft.world.level.biome.BiomeSource;
@@ -27,6 +29,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -53,39 +56,60 @@ public class StructureDebugMixins {
             method = "loadElementFromResource",
             at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/Decoder;parse(Lcom/mojang/serialization/DynamicOps;Ljava/lang/Object;)Lcom/mojang/serialization/DataResult;")
         )
-        private static DataResult<?> loadRegistryContents(
-                Decoder<?> instance, DynamicOps<?> ops, Object jsonElement, Operation<DataResult<?>> original,
-                @Local(argsOnly = true) WritableRegistry<?> registry
+        private static <E> DataResult<E> loadRegistryContents(
+                Decoder<E> instance, DynamicOps<Tag> ops, Object jsonElement, Operation<DataResult<E>> original,
+                @Local(argsOnly = true) WritableRegistry<E> registry
         ) {
-            return registryWrapper(instance, ops, jsonElement, original, registry);
+            return growsseth$registryWrapper(instance, ops, jsonElement, original, registry);
         }
 
         @WrapOperation(
             method = "loadContentsFromNetwork",
             at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/Decoder;parse(Lcom/mojang/serialization/DynamicOps;Ljava/lang/Object;)Lcom/mojang/serialization/DataResult;")
         )
-        private static DataResult<?> loadRegistryContentsFromNetwork(
-                Decoder<?> instance, DynamicOps<?> ops, Object jsonElement, Operation<DataResult<?>> original,
-                @Local(argsOnly = true) WritableRegistry<?> registry
+        private static <E> DataResult<E> loadRegistryContentsFromNetwork(
+                Decoder<E> instance, DynamicOps<Tag> ops, Object jsonElement, Operation<DataResult<E>> original,
+                @Local(argsOnly = true) WritableRegistry<E> registry
         ) {
-            return registryWrapper(instance, ops, jsonElement, original, registry);
+            return growsseth$registryWrapper(instance, ops, jsonElement, original, registry);
+        }
+
+
+        // NOTE: equivalent code to this crashed when upgrading to 1.21 (or 1.21.1?) as it switched
+        // from StructureSet to Optional<StructureSet> in vanilla code, failing the cast
+        // Check here if it fails again!
+        @Unique
+        private static <E> DataResult<E> growsseth$registryWrapper(Decoder<E> instance, DynamicOps<Tag> ops, Object jsonElement, Operation<DataResult<E>> original, WritableRegistry<E> registry) {
+            var result = original.call(instance, ops, jsonElement);
+            var key = registry.key();
+            if (DebugConfig.structuresDebugMode && key.equals(Registries.STRUCTURE_SET) && jsonElement.toString().contains("growsseth")) {
+                result = growsseth$alterStructureSetWeight(key, result);
+            }
+            return result;
         }
 
         @Unique
-        private static DataResult<?> registryWrapper(Decoder<?> instance, DynamicOps<?> ops, Object jsonElement, Operation<DataResult<?>> original, WritableRegistry<?> registry) {
-            var result = original.call(instance, ops, jsonElement);
-            if (DebugConfig.structuresDebugMode && registry.key().equals(Registries.STRUCTURE_SET) && jsonElement.toString().contains("growsseth")) {
-                RuinsOfGrowsseth.getLOGGER().info("(debug mode) Network | Increasing spawn frequency for {}", jsonElement);
-                StructureSet structureSet = (StructureSet) result.getOrThrow();
-                var placement = structureSet.placement();
-                placement.frequency = 1;
-                if (placement instanceof RandomSpreadStructurePlacement randomSpread) {
-                    randomSpread.spacing = randomSpread.spacing / 4;
-                    randomSpread.separation = randomSpread.separation / 4;
-                }
-                result = DataResult.success(structureSet);
+        private static <E> DataResult<E> growsseth$alterStructureSetWeight(ResourceKey<?> key, DataResult<?> dataResult) {
+            RuinsOfGrowsseth.LOGGER.info("(debug mode) Network | Increasing spawn frequency for {}", key);
+            Optional<?> structureSetOpt = growsseth$assertCast(dataResult.getOrThrow(), Optional.class);
+            StructureSet structureSet = growsseth$assertCast(structureSetOpt.orElseThrow(), StructureSet.class);
+            var placement = structureSet.placement();
+            placement.frequency = 1;
+            if (placement instanceof RandomSpreadStructurePlacement randomSpread) {
+                randomSpread.spacing = randomSpread.spacing / 4;
+                randomSpread.separation = randomSpread.separation / 4;
             }
-            return result;
+            //noinspection unchecked
+            return (DataResult<E>) DataResult.success(Optional.of(structureSet));
+        }
+
+        @Unique
+        private static <E> E growsseth$assertCast(Object value, Class<E> clazz) {
+            if (!(value.getClass().isAssignableFrom(clazz))) {
+                throw new IllegalArgumentException("Expected " + value + " to be an instance of " + clazz.getCanonicalName());
+            }
+            //noinspection unchecked
+            return (E) value;
         }
     }
 
@@ -106,7 +130,7 @@ public class StructureDebugMixins {
         )
         private void overrideStructuresInDebug(HolderLookup<StructureSet> structureSetLookup, RandomState randomState, long seed, CallbackInfoReturnable<ChunkGeneratorStructureState> cir) {
             if (DebugConfig.structuresDebugMode) {
-                RuinsOfGrowsseth.getLOGGER().info("(debug mode) Replaced flat worldgen structure selection");
+                RuinsOfGrowsseth.LOGGER.info("(debug mode) Replaced flat worldgen structure selection");
                 cir.setReturnValue(super.createState(structureSetLookup, randomState, seed));
             }
         }
