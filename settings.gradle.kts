33f362f91e17d1c844d14eb14d0babc8283b52e0
costantin0 <86307814+costantin0@users.noreply.github.com>
2025-11-29 16:52:58 +0100

    removed reference to Radical Cobblemon Trainers API folder

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -36,7 +36,3 @@ listOf(
     "neoforge",
     "fabric",
 ).forEach { include(it) }
-
-// Include the local Radical Cobblemon Trainers API build so we can compile directly against it
-// without modifying the API module itself.
-includeBuild("../api")
