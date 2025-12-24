be07327fcafc6b68242ed279b1f26dc6a0a44de5
costantin0 <86307814+costantin0@users.noreply.github.com>
2025-12-24 15:45:51 +0100

    1.21.3: Fixed researcher horn item

--- base/src/main/kotlin/com/ruslan/growsseth/item/ResearcherHornItem.kt
+++ base/src/main/kotlin/com/ruslan/growsseth/item/ResearcherHornItem.kt
@@ -5,4 +5,4 @@ import net.minecraft.world.item.Instrument
 import net.minecraft.world.item.InstrumentItem
 
 class ResearcherHornItem(properties: Properties, instruments: TagKey<Instrument>)
-    : InstrumentItem(properties, instruments)
+    : InstrumentItem(instruments, properties)
