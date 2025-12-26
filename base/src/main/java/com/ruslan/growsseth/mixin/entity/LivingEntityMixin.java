af29fa2a02727dc430e3177aa5afd1c57722f728
costantin0 <86307814+costantin0@users.noreply.github.com>
2025-12-26 17:51:37 +0100

    1.21.3: fixed LivingEntityMixin

--- base/src/main/java/com/ruslan/growsseth/mixin/entity/LivingEntityMixin.java
+++ base/src/main/java/com/ruslan/growsseth/mixin/entity/LivingEntityMixin.java
@@ -1,6 +1,7 @@
 package com.ruslan.growsseth.mixin.entity;
 
 import com.ruslan.growsseth.effect.GrowssethEffects;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
@@ -15,8 +16,8 @@ public abstract class LivingEntityMixin {
     @Unique
     LivingEntity entity = (LivingEntity)(Object)this;
 
-    @ModifyVariable(method = "hurt", at = @At(value = "HEAD"), ordinal = 0, argsOnly = true)
-    private float applyGrowssethEffects(float amount, DamageSource source) {
+    @ModifyVariable(method = "hurtServer", at = @At(value = "HEAD"), ordinal = 0, argsOnly = true)
+    private float applyGrowssethEffects(float amount, ServerLevel level, DamageSource source) {
         // The effects are applied before any other computation, they do not affect void and /kill damage but affect starve damage
         if (!source.is(DamageTypeTags.BYPASSES_INVULNERABILITY)) {
             if (entity.hasEffect(GrowssethEffects.INSTANCE.getSERENITY()) &&        // attacks from creative players bypass the serenity effect
