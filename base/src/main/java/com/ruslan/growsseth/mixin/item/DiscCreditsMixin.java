bf416b9226898a4d5e0127d70686caded87f83a6
costantin0 <86307814+costantin0@users.noreply.github.com>
2026-01-05 15:25:38 +0100

    1.21.11: temporarily disabled DiscCreditsMixin

--- base/src/main/java/com/ruslan/growsseth/mixin/item/DiscCreditsMixin.java
+++ base/src/main/java/com/ruslan/growsseth/mixin/item/DiscCreditsMixin.java
@@ -15,16 +15,17 @@ import java.util.function.Consumer;
 
 @Mixin(JukeboxPlayable.class)
 public class DiscCreditsMixin {
-    // Inside addToTooltip
-    @SuppressWarnings("UnresolvedMixinReference")
-    @Inject(
-        method = { "lambda$addToTooltip$1", "method_60748" },
-        at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V")
-    )
-    private static void addExtraCredits(Consumer<Component> tooltipAdder, Holder<JukeboxSong> holder, CallbackInfo ci) {
-        if (holder.unwrapKey().map(GrowssethJukeboxSongs.credits::containsKey).orElse(false)) {
-            var credits = GrowssethJukeboxSongs.credits.get(holder.unwrapKey().orElseThrow());
-            Optional.ofNullable(credits.getTooltip()).ifPresent(x -> x.forEach(tooltipAdder));
-        }
-    }
+    // todo: fix
+//    // Inside addToTooltip
+//    @SuppressWarnings("UnresolvedMixinReference")
+//    @Inject(
+//        method = { "lambda$addToTooltip$1", "method_60748" },
+//        at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V")
+//    )
+//    private static void addExtraCredits(Consumer<Component> tooltipAdder, Holder<JukeboxSong> holder, CallbackInfo ci) {
+//        if (holder.unwrapKey().map(GrowssethJukeboxSongs.credits::containsKey).orElse(false)) {
+//            var credits = GrowssethJukeboxSongs.credits.get(holder.unwrapKey().orElseThrow());
+//            Optional.ofNullable(credits.getTooltip()).ifPresent(x -> x.forEach(tooltipAdder));
+//        }
+//    }
 }
